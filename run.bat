08e03e7e49b1577c3dafa54cfe59bf2be525e3b6
wltjrs1234-JSK <wltjrs1234@gmail.com>
2026-07-02 14:56:20 +0900

    Fix: Correct VBScript quotes escaping using Chr(34) to support whitespace in paths

--- run.bat
+++ run.bat
@@ -1,6 +1,6 @@
 @echo off
 echo Set WshShell = CreateObject("WScript.Shell") > "%temp%\run_silent.vbs"
-echo WshShell.Run """%~dp0run_server.bat""", 0, False >> "%temp%\run_silent.vbs"
+echo WshShell.Run Chr(34) ^& "%~dp0run_server.bat" ^& Chr(34), 0, False >> "%temp%\run_silent.vbs"
 wscript.exe "%temp%\run_silent.vbs"
 del "%temp%\run_silent.vbs"
 exit /b
