3798dd9a6024249c2ec841acc5fae617de834e3f
wltjrs1234-JSK <wltjrs1234@gmail.com>
2026-07-02 15:01:30 +0900

    Fix: Add automatic browser startup and delay to run.bat for enhanced launching feedback

--- run.bat
+++ run.bat
@@ -3,4 +3,10 @@ echo Set WshShell = CreateObject("WScript.Shell") > "%temp%\run_silent.vbs"
 echo WshShell.Run Chr(34) ^& "%~dp0run_server.bat" ^& Chr(34), 0, False >> "%temp%\run_silent.vbs"
 wscript.exe "%temp%\run_silent.vbs"
 del "%temp%\run_silent.vbs"
+
+:: Wait briefly for FastAPI backend startup
+timeout /t 2 /nobreak >nul
+
+:: Launch default web browser to the dashboard URL
+start http://127.0.0.1:8500
 exit /b
